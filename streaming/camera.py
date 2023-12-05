745e1cf78b86c677643f170eee1adf1851245080
Polar <tpwns73@gmail.com>
2023-12-05 09:30:04 +0900

    frame delay check

--- streaming/camera.py
+++ streaming/camera.py
@@ -14,8 +14,8 @@ class RtspCam(object):
         
     
     def get_frame(self):
-        success, image = self.url.read()
         self.frame_cnt += 1
+        success, image = self.url.read()
         if self.frame_cnt % 10 == 0:
             ret, jpeg = cv2.imencode('.jpg', image)
             self.frame_cnt = 0
@@ -23,7 +23,7 @@ class RtspCam(object):
     '''
     def get_frame(self):
         success, image = self.url.read()
-        resize = cv2.resize(image, (640, 480), interpolation=cv2.INTER_LINEAR)
-        ret, jpeg = cv2.imencode('.jpg', resize)
+        #resize = cv2.resize(image, (640, 480), interpolation=cv2.INTER_LINEAR)
+        ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
     '''
